965b32a0bd316da0c62e42d3ca92b20ec61b1d37
GaneshBirajdar1399 <ganeshbirajdar321@gmail.com>
2025-01-16 16:15:55 +0530

    add build

--- onlineMobileStore/src/environments/environment.prod.ts
+++ onlineMobileStore/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,  // Indicates that this is the production environment
-    apiUrl: environment.apiUrl  // Live API for production
+  production: true,  // Indicates that this is the development environment
+  apiUrl: 'https://ecom-db-json.onrender.com'  // Local API for development
   };
   
